0324714d6b82f8d0c2073dfe43fda4f7ecfe5fcf
FrankScarlet <hferfrank@gmail.com>
2021-08-21 23:06:54 +0800

    add neog6.js: 节点转换完毕

--- README.md
+++ README.md
@@ -2,8 +2,48 @@
 
 > 本项目的初衷，参考资料的第一条
 
+## 格式
+
+目标格式
+
+```javascript
+const data = {
+  // 点集
+  nodes: [
+    {
+      id: "node1",
+      x: 100,
+      y: 200,
+    },
+    {
+      id: "node2",
+      x: 300,
+      y: 200,
+    },
+  ],
+  // 边集
+  edges: [
+    // 表示一条从 node1 节点连接到 node2 节点的边
+    {
+      source: "node1",
+      target: "node2",
+    },
+  ],
+};
+```
+
+## cheatsheet
+
+```bash
+npm install @antv/graphin@latest --save
+npm install @antv/graphin-components@latest --save
+npm install @antv/graphin-icons --save
+```
+
 ## 资料
 
-1.[neo4j-g6 讨论](https://github.com/neo4j-contrib/neovis.js/issues/192)
-2.[neovis](https://github.com/neo4j-contrib/neovis.js/blob/master/src/neovis.js)
-3.[motif](https://neo4j.com/developer-blog/creating-a-neo4j-react-app-with-use-neo4j-hooks/)
+1.[neo4j-g6 讨论](https://github.com/neo4j-contrib/neovis.js/issues/192) 
+2.[neovis](https://github.com/neo4j-contrib/neovis.js/blob/master/src/neovis.js) 
+3.[motif](https://neo4j.com/developer-blog/creating-a-neo4j-react-app-with-use-neo4j-hooks/) 
+4.[neo4j-graphist](https://neo4j.com/graphgists/)
+5.[graphin](https://graphin.antv.vision/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,36 @@
-import { Record} from 'neo4j-driver';
-import { isNode, isRelationship} from 'neo4j-driver-core'
-import React from 'react';
-import { useReadCypher } from 'use-neo4j'
-import './App.css';
-
-export const buildNode =(n: any) =>{
-  console.log('get a node')
-}
-
-export const buildEdge = (e: any) => {
-  console.log('get a edge')
-}
-
-export const toG6Format = (records: Record[])  => {
-  let nodes = {}
-  let edges= {}
-  // console.log(records)
-
-  records.forEach((record) => {
-    Object.values(record.toObject()).map(async (v) => {
-      if (isNode(v)) {
-        let node = buildNode(v);
-        
-      } else if (isRelationship(v)) {
-        let edge = buildEdge(v);
-      } else if (v instanceof Array) {
-        for (let obj of v) {
-          if (isNode(obj)) {
-            let node = buildNode(obj);
-          } else if (isRelationship(obj)) {
-            let edge = buildEdge(obj);
-          }
-        }
-      } else {
-        console.log('invalid format')
-      }
-    })
-  })
-} 
+import React from "react";
+import { useReadCypher } from "use-neo4j";
+import "./App.css";
+import NeoG6 from "./neog6";
+import Graphin from "@antv/graphin";
+import { Toolbar } from "@antv/graphin-components";
 
 function App() {
   // const { cypher, error, loading, first} = useReadCypher('MATCH (n) RETURN count(n) AS count')
-  const { cypher, error, loading, first, records} = useReadCypher('MATCH (n:Person)-[re]-(b) RETURN n, re, b LIMIT 10')
+  const { cypher, error, loading, first, records } = useReadCypher(
+    "MATCH (n:Person)-[re]-(b) RETURN n, re, b LIMIT 10"
+  );
   // Default to Loading Message
-  let result = (<div className="ui active dimmer">Loading...</div>)
-
+  let result = <div className="ui active dimmer">Loading...</div>;
+  let g6 = new NeoG6();
+  let temp;
   // Was there an error om the query?
-  if ( error ) {
-    result = (<div className="ui negative message">{ error.message }</div>)
-  }
-  else if ( !loading ) {
-    // Get the count
-    // console.log(records)
+  if (error) {
+    result = <div className="ui negative message">{error.message}</div>;
+  } else if (!loading) {
     if (records instanceof Array) {
-      toG6Format(records)
+      g6.toG6Format(records)
     }
-
-    // const count = first?.get('count').toNumber()
-    const count = 114514 
-    result = (<div>There are {count} nodes in the database.</div>)
+    // Get the count
+    const count = first?.get('count').toNumber()
+    result = (
+      <div>
+        There are {count} nodes in the database.
+        <Graphin data={g6.data} layout={{ name: "concentric" }}>
+          <Toolbar />
+        </Graphin>
+      </div>
+    );
   }
 
   return (
